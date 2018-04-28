651fd16af056806753950dcc744067f896c3972e
cvbutani <chirag.butani1@gmail.com>
2018-04-28 15:03:54 -0400

    Inital Commit

---
+++ src/com/cvbutani/ThreadColor.java
@@ -0,0 +1,18 @@
+package com.cvbutani;
+
+/**
+ * Project name: ProducerConsumer
+ * Created by: cvbutani
+ * Date: 28/04/18
+ */
+
+public class ThreadColor {
+    static final String ANSI_RESET = "\u001B[0m";
+    static final String ANSI_BLACK = "\u001B[30m";
+    static final String ANSI_RED = "\u001B[31m";
+    static final String ANSI_GREEN = "\u001B[32m";
+    static final String ANSI_BLUE = "\u001B[34m";
+    static final String ANSI_PURPLE = "\u001B[35m";
+    static final String ANSI_CYAN = "\u001B[36m";
+
+}
